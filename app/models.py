8770bf03f4869cecc4acf0a2f08c4b085d666c6a
juz410 <kokfeengtan@gmail.com>
2022-07-12 11:15:38 +0800

    remove unncessary import

--- app/models.py
+++ app/models.py
@@ -1,5 +1,4 @@
-from cgitb import text
-from enum import unique
+
 from sqlalchemy import TIMESTAMP, Column, ForeignKey,Integer,String, Boolean, false
 from sqlalchemy.sql.expression import text
 from sqlalchemy.orm import relationship
