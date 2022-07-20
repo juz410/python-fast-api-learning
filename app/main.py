a5cb3696e9c987f290114090bc1ff1ac3e509ed8
juz410 <kokfeengtan@gmail.com>
2022-07-20 16:40:00 +0800

    hello andrew

--- app/main.py
+++ app/main.py
@@ -28,7 +28,7 @@ app.include_router(vote.router)
 
 @app.get('/')
 def hello_world():
-    return {"message": "Hello World Succesfully from CICD pipeline"}
+    return {"message": "Hello Andrew"}
 
 
 
