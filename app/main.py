bb487fa9f89178399bfa7c5c2f17ea2d6f38f867
juz410 <kokfeengtan@gmail.com>
2022-10-11 14:54:58 +0800

    testing write file

--- app/main.py
+++ app/main.py
@@ -4,10 +4,15 @@ from fastapi.middleware.cors import CORSMiddleware
 from .routers import post,user,auth,vote
 from .database import engine
 from .config import Settings
+from pydantic import BaseModel
 
 
 # Since already got alembic, so no need use this to create any table
 # models.Base.metadata.create_all(bind=engine)
+class Post(BaseModel):
+    title: str
+    content: str
+
 
 app = FastAPI()
 
@@ -29,6 +34,17 @@ app.include_router(vote.router)
 @app.get('/')
 def hello_world():
     return {"message": "Hello Desmond"}
+
+
+
+
+@app.post("/testing_api")
+def testing_api(post: Post):
+    print(post)
+    file = open("test.txt","a")
+    file.write(post.title + "|" + post.content+"\n")
+    return post
+
 
 
 
